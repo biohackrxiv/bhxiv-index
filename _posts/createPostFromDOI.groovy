cdcbaffd000465979350b86fa740f0b8537a7699
Egon Willighagen <egon.willighagen@gmail.com>
2025-05-18 12:01:35 +0200

    First draft of a tool that create preprint pages based on CrossRef metadata

---
+++ _posts/createPostFromDOI.groovy
@@ -0,0 +1,59 @@
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='1.0.5')
+
+import groovy.json.JsonSlurper
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+workspaceRoot = ".."
+ui = new net.bioclipse.managers.UIManager(workspaceRoot);
+bioclipse = new net.bioclipse.managers.BioclipseManager(workspaceRoot);
+
+if (args.length < 2) {
+  println "Syntax: groovy createPostFromDOI.groovy <DOI> <BH_CODE>"
+  System.exit(0)
+}
+
+doi = args[0]
+tag = args[1]
+
+localpart = doi.split("/osf.io/")[1].split("_")[0]
+
+bioclipse = new net.bioclipse.managers.BioclipseManager(".")
+ui = new net.bioclipse.managers.UIManager(".")
+
+def parser = new JsonSlurper()
+
+metadataContent = bioclipse.download(
+  "https://doi.org/" + doi, "application/vnd.citationstyles.csl+json"
+)
+
+metadata = parser.parseText(metadataContent)
+
+date = metadata.created."date-time".substring(0,10)
+
+postFile = "/" + date + "-" + localpart + ".markdown"
+ui.renewFile(postFile)
+
+header = """---
+layout: post
+title:  "${metadata.title}"
+date:   ${date}
+tags: ${tag}
+doi: ${doi} ${localpart}
+authors:
+"""
+
+ui.append(postFile, header)
+
+for (author in metadata.author) {
+  ui.append(postFile, "  - name: ${author.given} ${author.family}\n")
+  if (author."authenticated-orcid") {
+    orcid = author.ORCID.replace("https://orcid.org/","")
+    ui.append(postFile, "  - orcid: ${orcid}\n")
+  }
+}
+
+ui.append(postFile, "---\n\n")
+
+ui.append(postFile, metadata."abstract".replace("<p>","").replace("</p>","\n\n"))
+
