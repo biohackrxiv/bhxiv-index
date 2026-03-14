868c6a29197f09d0249cbb6cbab6a9e3bb01b66e
Egon Willighagen <egon.willighagen@gmail.com>
2026-03-14 12:22:54 +0100

    New report

--- _posts/createPostFromDOI.groovy
+++ _posts/createPostFromDOI.groovy
@@ -1,4 +1,4 @@
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='1.0.9-SNAPSHOT')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='1.0.9')
 
 import groovy.json.JsonSlurper
 import java.text.SimpleDateFormat;
